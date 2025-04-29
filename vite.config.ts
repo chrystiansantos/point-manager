853774ffe5b57fd3a931727bd26de753d88d9472
Chrystian Santos <chrystian_santos_silva@hotmail.com>
2025-04-28 21:57:53 -0300

    feat: config inter font and tailwind css

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,31 @@
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
+import Unfonts from 'unplugin-fonts/vite';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
   const { VITE_PORT } = loadEnv(mode, process.cwd(), '');
   return {
-    plugins: [react()],
+    plugins: [
+      react(),
+      tailwindcss(),
+      Unfonts({
+        google: {
+          families: [{
+            name: 'Inter',
+            styles: 'wght@400;500;700',
+          }],
+        },
+      }),
+    ],
     server: {
-      port: Number(VITE_PORT),
+      port: Number(VITE_PORT)
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
   };
 });
